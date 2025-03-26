e7e9ab5344b8211913a39109f828648f9c1486fe
kanhaya kumar yadav <kanhayaky120@gmail.com>
2025-03-26 22:25:02 +0530

    Step 3: change client into chat client

--- realtime_chat/client.py
+++ realtime_chat/client.py
@@ -1,19 +1,41 @@
+import sys
+from select import select
+
 from socket import socket, AF_INET, SOCK_STREAM
 from constants import HOST, PORT
 
 
 with socket(AF_INET, SOCK_STREAM) as client_sock:
     client_sock.connect((HOST, PORT))
+    client_sock.setblocking(False)
 
-    while True:
-        message = input("Send: ")
+    inputs = [client_sock, sys.stdin]
+    outputs = []
 
-        if message.lower() in ["quit", "exit"]:
-            break
+    exit = False
+    name = input("Name: ")
+    print(f"{name}: ", end="", flush=True)
 
-        client_sock.send(message.encode())
+    while not exit:
 
-        response = client_sock.recv(1024)
-        print(f"Recv: {response.decode()}")
+        readable, writeable, exceptional = select(inputs, outputs, inputs)
+
+        for source in readable:
+            if source is client_sock:
+                response = client_sock.recv(1024)
+                print(f"Serv: {response.decode()}")
+                print(f"{name}: ", end="", flush=True)
+
+            else:
+                message = sys.stdin.readline().strip()
+
+                if message.lower() in ["quit", "exit"]:
+                    exit = True
+                else:
+                    outputs.append(client_sock)
+
+        for source in writeable:
+            source.send(message.encode())
+            outputs.remove(source)
 
     client_sock.close()
